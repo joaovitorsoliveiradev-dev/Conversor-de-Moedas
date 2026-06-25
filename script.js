b85076fed7e218e86afa02c2e663120f6c1c1b33
joaovitorsoliveiradev-dev <joao.vitor.soliveiradev@gmail.com>
2026-06-25 20:15:14 -0300

    Estrutura Inicial

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 function HandleConverterValue(){
-    let Moeda = parseInt(document.querySelector("#Moeda").value);
+    let coin = parseInt(document.querySelector("#coin").value);
     let operation = document.querySelector("select").value
     let convertion = document.querySelector("h2")
     
+    if(coin === 0 || isNaN(coin)){
+        alert("Digite um valor válido")
+    return}
+    
     switch(operation) {
         case "USD":
-            conversao = Moeda * 5.19
-            convertion.innerHTML = `Na cotação atual, ${Moeda} BRL equivale a ${conversao.toFixed(2)} USD`;
+            conversao = coin * 5.19
+            convertion.innerHTML = `Na cotação atual, ${coin} BRL equivale a ${conversao.toFixed(2)} USD`;
             break;
         case "EUR":
-            conversao = Moeda * 5.91; 
-            convertion.innerHTML = `Na cotação atual, ${Moeda} BRL equivale a ${conversao.toFixed(2)} EUR`;
+            conversao = coin * 5.91; 
+            convertion.innerHTML = `Na cotação atual, ${coin} BRL equivale a ${conversao.toFixed(2)} EUR`;
             break;
         case "GBP":
-            conversao = Moeda * 6.85; 
-            convertion.innerHTML = `Na cotação atual, ${Moeda} BRL equivale a ${conversao.toFixed(2)} GBP`;
+            conversao = coin * 6.85; 
+            convertion.innerHTML = `Na cotação atual, ${coin} BRL equivale a ${conversao.toFixed(2)} GBP`;
             break;
     }
 }
